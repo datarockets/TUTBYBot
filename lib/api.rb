956bbce6c845f2db493e286012328a8f7aeb8ceb
alekssenkou <aleksey.senkou@gmail.com>
2016-08-23 21:38:43 +0300

    Move to_kson logic to apiRequest method

--- lib/api.rb
+++ lib/api.rb
@@ -1,52 +1,53 @@
 require 'net/http'
 require 'json'
+require 'pry'
 require_relative '../config/config'
 
 module API
   def news_category_handler(category_id, id)
-    json = {
+    params = {
       jsonrpc: "2.0",
       method: "/tutby/categories/updates_list",
       params: {
         items: [{
           categoryId: category_id,
-          updated: Time.now.to_i.to_s
+          updated: Time.now
         }]
       }, id: id
-    }.to_json
+    }
 
-    apiRequest(json)
+    apiRequest(params)
   end
 
   def main_handler(type)
     case type
       when 'top'
-        json = {
+        params = {
           jsonrpc: "2.0",
           method: "/tutby/top5",
           id: 6,
           params: {
             limit: 5
           }
-        }.to_json
+        }
       when 'now'
-        json = {
+        params = {
           jsonrpc: "2.0",
           method: "/tutby/news/popular",
           id: 4,
           params: {
             count: "5"
           }
-        }.to_json
+        }
     end
 
-    apiRequest(json)
+    apiRequest(params)
   end
 
   def search_news(query)
     categories = %w(50 99011 10 9 310 11 3 6 5 336 15 7 103 16 486 491 51)
 
-    json = {
+    params = {
       jsonrpc: "2.0",
       method: "/tutby/news/search",
       i: "15",
@@ -54,22 +55,22 @@ module API
         categories: categories,
         text: query
       }
-    }.to_json
+    }
 
-    apiRequest(json)
+    apiRequest(params)
   end
 
   def get_news(news_array)
-    json = {
+    params = {
       jsonrpc: "2.0",
       method: "/tutby/news/data",
       id: "16",
       params: {
         ids: news_array
       }
-    }.to_json
+    }
 
-    apiRequest(json)
+    apiRequest(params)
   end
 
   def finance_request
@@ -93,12 +94,12 @@ module API
 
   private
 
-    def apiRequest(json)
-      request = Net::HTTP::Post.new(Config::NEWS_METHOD, initiheader = { 'Content-Type' => 'application/json' })
-      request.body = json
+    def apiRequest(params)
+      request = Net::HTTP::Post.new(Config::NEWS_METHOD)
+      request.body = params.to_json
       response = Net::HTTP.new(Config::NEWS_SERVER, Config::PORT)
         .start {|http| http.request(request)}
 
       JSON.parse(response.body)
-  end
+    end
 end
