51856a6111e1548882cd2fbabf519c7130c094a7
alekssenkou <aleksey.senkou@gmail.com>
2016-08-24 22:58:18 +0300

    Remove useless duplicate chat id

--- bot/action.rb
+++ bot/action.rb
@@ -8,103 +8,103 @@ module Bot
     def initialize(bot:, user_message:)
       @bot = bot
       @user_message = user_message
+      @id = @user_message.chat.id
       @messages = YAML::load(IO.read(messages_path))
     end
 
     def run
-      id = @user_message.chat.id
       case @user_message.text
         when '/start'
-          track_event('Новый пользователь', id)
-          send_response(id, @messages['start_using'])
+          track_event('Новый пользователь', @id)
+          send_response(@messages['start_using'])
 
         when '/help'
-          track_event('Новый пользователь', id)
-          send_response(id, @messages['help'])
+          track_event('Новый пользователь')
+          send_response(@messages['help'])
 
         when '/author'
-          track_event('Автор', id)
-          send_response(id, @messages['author'])
+          track_event('Автор')
+          send_response(@messages['author'])
 
         when '/search'
-          track_event('Попытка поиска', id)
-          send_response(id, @messages['try_search'])
+          track_event('Попытка поиска')
+          send_response(@messages['try_search'])
 
         when /search/i
           split = @user_message.text.split(" ")
           unless split[1].nil?
             query = split[1]
-            search_for_news(id, query)
+            search_for_news(query)
           end
 
         when '/top'
-          last_news_getter(id, "Топ-5 новостей", "top")
+          last_news_getter("Топ-5 новостей", "top")
 
         when '/now'
-          last_news_getter(id, "Последние новости", "now")
+          last_news_getter("Последние новости", "now")
 
         when '/politics'
-          news_category_getter(id, "Политика", "10", 86)
+          news_category_getter("Политика", "10", 86)
 
         when '/economics'
-          news_category_getter(id, "Экономика", "9", 39)
+          news_category_getter("Экономика", "9", 39)
 
         when '/finance'
-          news_category_getter(id, "Финансы", "310", 41)
+          news_category_getter("Финансы", "310", 41)
 
         when '/society'
-          news_category_getter(id, "Общество", "11", 43)
+          news_category_getter("Общество", "11", 43)
 
         when '/world'
-          news_category_getter(id, "Мировые новости", "3", 49)
+          news_category_getter("Мировые новости", "3", 49)
 
         when '/sports'
-          news_category_getter(id, "Спорт", "6", 53)
+          news_category_getter("Спорт", "6", 53)
 
         when '/culture'
-          news_category_getter(id, "Культура", "5", 57)
+          news_category_getter("Культура", "5", 57)
 
         when '/42'
-          news_category_getter(id, "42", "15", 65)
+          news_category_getter("42", "15", 65)
 
         when '/auto'
-          news_category_getter(id, "Автоновости", "7", 69)
+          news_category_getter("Автоновости", "7", 69)
 
         when '/accidents'
-          news_category_getter(id, "Происшествия", "103", 73)
+          news_category_getter("Происшествия", "103", 73)
 
         when '/property'
-          news_category_getter(id, "Недвижимость", "486", 79)
+          news_category_getter("Недвижимость", "486", 79)
 
         when '/agenda'
-          news_category_getter(id, "Афиша", "491", 98)
+          news_category_getter("Афиша", "491", 98)
 
         when '/kurs'
-          currencies_getter(id, "Курсы валют")
+          currencies_getter("Курсы валют")
       end
     end
 
     private
 
-      def send_response(id, response)
-        @bot.api.sendMessage(chat_id: id, text: response)
+      def send_response(response)
+        @bot.api.sendMessage(chat_id: @id, text: response)
       end
 
-      def track_event(id, event)
-        @bot.track(event, id, type_of_chat: id)
+      def track_event(event)
+        @bot.track(event, @id, type_of_chat: @id)
       end
 
-      def news_category_getter(chat, title, category, id)
-        track_event(chat, title)
-        response = news_category_handler(category, id)
+      def news_category_getter(title, category, category_id)
+        track_event(title)
+        response = news_category_handler(category, category_id)
         news = response['result'].each do |result|
           items = result['items']
-          news_sender(items, chat)
+          news_sender(items)
         end
       end
 
-      def search_for_news(chat, query)
-        track_event(chat, "Поиск новостей по фразе #{query}")
+      def search_for_news(query)
+        track_event("Поиск новостей по фразе #{query}")
 
         response = search_news(query)
         news = response['result']['items']
@@ -113,32 +113,32 @@ module Bot
         news_response = get_news(news_ids)
 
         new_response = news_response['result']['items']
-        news_sender(new_response, chat)
+        news_sender(new_response)
       end
 
-      def last_news_getter(chat_id, title, category)
-        track_event(chat_id, title)
+      def last_news_getter(title, category)
+        track_event(title)
         response = main_handler(category)
         news = response['result']['items']
-        news_sender(news, chat_id)
+        news_sender(news)
       end
 
-      def currencies_getter(chat_id, title)
-        track_event(chat_id, title)
+      def currencies_getter(title)
+        track_event(title)
         response = finance_request
         currencies = response['exchangeRates']
-        currencies_sender(currencies, chat_id)
+        currencies_sender(currencies)
       end
 
-      def news_sender(news, id)
+      def news_sender(news)
         news[0..4].each do |item|
-          send_response(id, "#{item['title']} \n #{item['shortUrl']}")
+          send_response("#{item['title']} \n #{item['shortUrl']}")
         end
       end
 
-      def currencies_sender(currencies, id)
+      def currencies_sender(currencies)
         currencies[0..3].each do |currency|
-          send_response(id, "#{currency['currencyCode']} - #{currency['nb']}")
+          send_response("#{currency['currencyCode']} - #{currency['nb']}")
         end
       end
 
