1076d909bbd44a1f1c4ef34f9ca992e0e744f3f0
alekssenkou <aleksey.senkou@gmail.com>
2016-08-28 12:35:46 +0300

    Update Bot namespace

--- bot/client.rb
+++ bot/client.rb
@@ -3,23 +3,22 @@ require 'telegram/bot/botan'
 require_relative '../config/config'
 require_relative 'action'
 
-module Bot
-  class Client
-    def initialize(token = nil, botan_token = nil)
-      @token = token || Config::TOKEN
-      @botan_token = botan_token || Config::BOTAN_TOKEN
-    end
+class Bot::Client
+  def initialize(token = nil, botan_token = nil)
+    @token = token || Config::TOKEN
+    @botan_token = botan_token || Config::BOTAN_TOKEN
+  end
 
-    def start
-      Telegram::Bot::Client.run(@token) do |bot|
-        bot.enable_botan!(@botan_token)
+  def start
+    Telegram::Bot::Client.run(@token) do |bot|
+      bot.enable_botan!(@botan_token)
 
-        bot.listen do |user_message|
-          Bot::Action.new(
-            bot: bot,
-            user_message: user_message
-          ).run
-        end
+      bot.listen do |user_message|
+        Bot::Action.new(
+          bot: bot,
+          user_message: user_message
+        ).run
       end
     end
+  end
 end
